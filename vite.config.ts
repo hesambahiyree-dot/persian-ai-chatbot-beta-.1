138cefd0754279af67af9b49df0f899ea523c544
hesambahiyree-dot <hesambahiyree@gmail.com>
2026-09-20 00:15:57 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "node:path";
 
 export default defineConfig({
   base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react(),
